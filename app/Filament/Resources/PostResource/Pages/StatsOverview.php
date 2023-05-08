9f0bc6bd000eb03265d89335458d6491acd46a4b
cameramanbrayton <braytonscottproductions@gmail.com>
2023-05-08 17:38:30 -0500

    Filament Blog

---
+++ app/Filament/Resources/PostResource/Pages/StatsOverview.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\PostResource\Pages;
+
+use App\Models\Post;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            Card::make('All Posts', Post::all()->count())->description('32k increase')
+            ->descriptionIcon('heroicon-s-trending-up')
+            ->color('danger'),
+        ];
+    }
+}
